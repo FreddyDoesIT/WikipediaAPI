2b8bb98dde19f68866734a05bcaed38223c83cab
freddy <zhonglyu173@gmail.com>
2019-04-19 10:31:54 -0700

    config done

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import Hello from './Hello.jsx';
-import Info from './Info.jsx';
+import React, { Component } from "react";
 
-const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
-  </div>
-);
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Welcome to Meteor!</h1>
+			</div>
+		);
+	}
+}
 
-export default App;
