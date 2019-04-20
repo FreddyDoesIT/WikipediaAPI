80b6f43fbea83788e55bcaec248f35d5621c128b
freddy <zhonglyu173@gmail.com>
2019-04-19 22:14:29 -0700

    adding Routers and NotFound component

--- imports/routers/Routers.js
+++ imports/routers/Routers.js
@@ -1,1 +1,44 @@
-Routers.js
+import { Tracker } from "meteor/tracker";
+import { Meteor } from "meteor/meteor";
+import React from "react";
+import { Router, Route, Switch } from "react-router";
+import { createBrowserHistory } from "history";
+
+import App from "../ui/App.jsx";
+import NotFound from "../ui/NotFound.jsx";
+import SearchingHistory from "../ui/SearchingHistory.jsx";
+
+const browserHistory = createBrowserHistory();
+const authPages = ["/history"];
+
+// Tracking auth status
+const authStatus = isLoggedin => {
+	// get the current location
+	const pathname = browserHistory.location.pathname;
+
+	const isAuthPage = authPages.includes(pathname);
+
+	// if user on an authenticated page but not logged in, redirect to /
+	if (isAuthPage && !isLoggedin) {
+		browserHistory.replace("/");
+	}
+};
+
+// routers
+export const renderRoutes = () => (
+	<Router history={browserHistory}>
+		<Switch>
+			<Route exact path="/" component={App} />
+
+			<Route exact path="/history" component={SearchingHistory} />
+
+			<Route component={NotFound} />
+		</Switch>
+	</Router>
+);
+
+
+Tracker.autorun(() => {
+	const isLoggedin = !!Meteor.userId();
+	authStatus(isLoggedin);
+});
